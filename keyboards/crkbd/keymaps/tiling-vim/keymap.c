ec5bb9339ddf786ac4763112957ab3479b3f0e0f
mariofrescas <mario.frescas.marquez@gmail.com>
2021-01-05 01:19:53 -0600

    Update brackets position

--- keyboards/crkbd/keymaps/tiling-vim/keymap.c
+++ keyboards/crkbd/keymaps/tiling-vim/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       LSFT_T(KC_CAPS),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, RSFT_T(KC_QUOT),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  RCTL_T(KC_ENT),
+      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,  KC_SLSH, KC_COMM,  KC_DOT, RCTL_T(KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         KC_LGUI,   KC_LALT,  KC_ESC,     KC_SPC,   MO(1), MO(2)
                                       //`--------------------------'  `--------------------------'
@@ -49,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_RALT, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,                         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
+      KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,  KC_NO,                         KC_MINS,  KC_EQL, KC_BSLS, KC_LBRC, KC_RBRC, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_NO,  KC_NO,                         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_NO,    KC_NO,  KC_NO,    KC_NO,   KC_NO,  KC_NO,                         KC_UNDS, KC_PLUS, KC_PIPE, KC_LCBR, KC_RCBR, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LGUI,  KC_LALT,  KC_ESC,    KC_SPC, MO(3),   KC_NO
                                       //`--------------------------'  `--------------------------'
